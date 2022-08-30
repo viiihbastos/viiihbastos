24b67298c8768b759fa59d37ad6bbc5b8ec47936
VICTOR BASTOS <victorbastos26@hotmail.com>
2022-08-29 21:55:55 -0300

    ux

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,20 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Cara`,
+    // Default title of the page
+    siteTitleAlt: `Cara - @lekoarts/gatsby-theme-cara`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://cara.lekoarts.de`,
+    // Used for SEO
+    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@lekoarts_de`,
   },
-  plugins: [],
-}
-// gatsby-config.js
-module.exports = {
-  plugins: [
-    {
-      resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {
-        // basePath defaults to `/`
-        basePath: `/sideproject`,
-      },
-    },
-  ],
 };
